4c90265ebbff14479cf1b248fa6a20e3c2dc791c
Yuri Flores Albuquerque <yuri.falbu@gmail.com>
2023-02-22 16:53:23 -0300

    feat(shape_opt): experiments code pattern

--- shape_optimization/README.md
+++ shape_optimization/README.md
@@ -1,7 +1,7 @@
-# Run with firedrake singularity container
+# Running experiments
 Required: [Singularity](https://docs.sylabs.io/guides/3.5/user-guide/introduction.html) previously installed
 
-Build your container locally
+### Build your container image locally
 ============================
 
 To run the shape optimization experiments for sharp interface idenfication first you will need to build a container with [Firedrake](https://www.firedrakeproject.org/) and [SeismicMesh](https://github.com/krober10nd/SeismicMesh). The shell commands bellow will create a parent folder `project`, clone the source repository inside parent folder and build the singularity container `FS.sif`. The build step will take some time to finish since we are compiling firedrake from the source.    
@@ -12,7 +12,7 @@ git clone https://github.com/Yuri-Albuquerque/fem_level_set_opt.git
 sudo singularity build FS.sif /fem_level_set_opt/shape_optimization/recipe.def 
 ```
 
-Runnig experiments
+### Runnig experiments
 ======================
 
 From the parent directory run the command: 
--- shape_optimization/optimization/1-run_sharp_interface_optimization.py
+++ shape_optimization/optimization/1-run_sharp_interface_optimization.py
@@ -4,64 +4,7 @@ from firedrake import *
 
 import spyro
 import gc
-
 gc.disable()
-
-model = {}
-model["opts"] = {
-    "method": "KMV",
-    "degree": 1,  # p order
-    "dimension": 2,  # dimension
-    "quadrature": "KMV",
-}
-model["mesh"] = {
-    "Lz": 0.65,  # depth in km - always positive
-    "Lx": 1.00,  # width in km - always positive
-    "Ly": 0.0,  # thickness in km - always positive
-    "meshfile": "../../meshes/immersed_disk_guess_vp.msh",
-    "initmodel": "../../velocity_models/immersed_disk_guess_vp.hdf5",
-    "truemodel": "../../velocity_models/immersed_disk_true_vp.hdf5",
-}
-model["PML"] = {
-    "status": True,  # true,  # true or false
-    "outer_bc": "non-reflective",  #  dirichlet, neumann, non-reflective (outer boundary condition)
-    "damping_type": "polynomial",  # polynomial, hyperbolic, shifted_hyperbolic
-    "exponent": 2,
-    "cmax": 5.0,  # maximum acoustic wave velocity in pml - km/s
-    "R": 0.001,  # theoretical reflection coefficient
-    "lz": 0.50,  # thickness of the pml in the z-direction (km) - always positive
-    "lx": 0.50,  # thickness of the pml in the x-direction (km) - always positive
-    "ly": 0.0,  # thickness of the pml in the y-direction (km) - always positive
-}
-recvs = spyro.create_transect((-0.01, 0.01), (-0.01, 0.99), 100)
-sources = spyro.create_transect((-0.01, 0.01), (-0.01, 0.99), 4)
-model["acquisition"] = {
-    "source_type": "Ricker",
-    "num_sources": len(sources),
-    "source_pos": sources,
-    "frequency": 5.0,
-    "delay": 1.0,
-    "amplitude": 1.0,
-    "num_receivers": len(recvs),
-    "receiver_locations": recvs,
-}
-model["timeaxis"] = {
-    "t0": 0.0,  #  initial time for event
-    "tf": 1.0,  # final time for event
-    "dt": 0.0005,  # timestep size
-    "nspool": 9999,  # how frequently to output solution to pvds
-    "fspool": 10,  # how frequently to save solution to ram
-    "skip": 2,
-}
-model["parallelism"] = {
-    "type": "automatic",  # options: automatic (same number of cores for evey processor), custom, off
-    "custom_cores_per_shot": [],  # only if the user wants a different number of cores for every shot.
-    # input is a list of integers with the length of the number of shots.
-}
-#### end of options ####
-
-VP_1 = 4.5  # inside subdomain to be optimized
-VP_2 = 2.0  # outside subdomain to be optimized
 
 
 def calculate_indicator_from_vp(vp):
@@ -109,7 +52,8 @@ def create_weighting_function(V, const=100.0, M=5, width=0.1, show=False):
     m = V.ufl_domain()
     W2 = VectorFunctionSpace(m, V.ufl_element())
     coords = interpolate(m.coordinates, W2)
-    Z, X = coords.dat.data_ro_with_halos[:, 0], coords.dat.data_ro_with_halos[:, 1]
+    Z, X = coords.dat.data_ro_with_halos[:,
+                                         0], coords.dat.data_ro_with_halos[:, 1]
 
     a0 = np.amin(X)
     a1 = np.amax(X)
@@ -171,7 +115,7 @@ def calculate_functional(model, mesh, comm, vp, sources, receivers, iter_num):
                     + ".png"
                 )
                 plt.close()
-            ## END DEBUG
+            # END DEBUG
 
             residual = spyro.utils.evaluate_misfit(
                 model,
@@ -198,7 +142,8 @@ def calculate_gradient(model, mesh, comm, vp, guess, guess_dt, weighting, residu
     """Calculate the shape gradient"""
     if comm.ensemble_comm.rank == 0 and comm.comm.rank == 0:
         print("Computing the shape derivative...", flush=True)
-    VF = VectorFunctionSpace(mesh, model["opts"]["method"], model["opts"]["degree"])
+    VF = VectorFunctionSpace(
+        mesh, model["opts"]["method"], model["opts"]["degree"])
     theta = Function(VF, name="gradient")
     for sn in range(model["acquisition"]["num_sources"]):
         if spyro.io.is_owner(comm, sn):
@@ -338,23 +283,103 @@ def optimization(model, mesh, V, comm, vp, sources, receivers, max_iter=10):
     return vp
 
 
-# run the script
+def oneDiskOptimization():
+    # parameters
+    num_sources = 4
+    num_receivers = 100
+    meshfile = "../../meshes/immersed_disk_guess_vp.msh"
+    initmodel = "../../velocity_models/immersed_disk_guess_vp.hdf5"
+    truemodel = "../../velocity_models/immersed_disk_true_vp.hdf5"
 
-comm = spyro.utils.mpi_init(model)
+    model = {}
+    model["opts"] = {
+        "method": "KMV",
+        "degree": 1,  # p order
+        "dimension": 2,  # dimension
+        "quadrature": "KMV",
+    }
 
-mesh, V = spyro.io.read_mesh(model, comm)
+    model["mesh"] = {
+        "Lz": 0.65,  # depth in km - always positive
+        "Lx": 1.00,  # width in km - always positive
+        "Ly": 0.0,  # thickness in km - always positive
+        "meshfile": meshfile,
+        "initmodel": initmodel,
+        "truemodel": truemodel,
+    }
 
-vp = spyro.io.interpolate(model, mesh, V, guess=True)
+    model["PML"] = {
+        "status": True,  # true,  # true or false
+        # dirichlet, neumann, non-reflective (outer boundary condition)
+        "outer_bc": "non-reflective",
+        "damping_type": "polynomial",  # polynomial, hyperbolic, shifted_hyperbolic
+        "exponent": 2,
+        "cmax": 5.0,  # maximum acoustic wave velocity in pml - km/s
+        "R": 0.001,  # theoretical reflection coefficient
+        # thickness of the pml in the z-direction (km) - always positive
+        "lz": 0.50,
+        # thickness of the pml in the x-direction (km) - always positive
+        "lx": 0.50,
+        # thickness of the pml in the y-direction (km) - always positive
+        "ly": 0.0,
+    }
 
-# visualize the updates with this file
-if comm.ensemble_comm.rank == 0:
-    evolution_of_velocity = File("../../results/evolution_of_velocity.pvd", comm=comm.comm)
-    evolution_of_velocity.write(vp, name="velocity")
+    recvs = spyro.create_transect((-0.01, 0.01), (-0.01, 0.99), num_receivers)
+    sources = spyro.create_transect((-0.01, 0.01), (-0.01, 0.99), num_sources)
 
-# Configure the sources and receivers
-sources = spyro.Sources(model, mesh, V, comm).create()
+    model["acquisition"] = {
+        "source_type": "Ricker",
+        "num_sources": num_sources,
+        "source_pos": sources,
+        "frequency": 5.0,
+        "delay": 1.0,
+        "amplitude": 1.0,
+        "num_receivers": num_receivers,
+        "receiver_locations": recvs,
+    }
 
-receivers = spyro.Receivers(model, mesh, V, comm).create()
+    model["timeaxis"] = {
+        "t0": 0.0,  # initial time for event
+        "tf": 1.0,  # final time for event
+        "dt": 0.0005,  # timestep size
+        "nspool": 9999,  # how frequently to output solution to pvds
+        "fspool": 10,  # how frequently to save solution to ram
+        "skip": 2,
+    }
 
-# run the optimization based on a line search for max_iter iterations
-vp = optimization(model, mesh, V, comm, vp, sources, receivers, max_iter=50)
+    model["parallelism"] = {
+        # options: automatic (same number of cores for evey processor), custom, off
+        "type": "automatic",
+        # only if the user wants a different number of cores for every shot.
+        "custom_cores_per_shot": [],
+        # input is a list of integers with the length of the number of shots.
+    }
+    #### end of options ####
+
+    VP_1 = 4.5  # inside subdomain to be optimized
+    VP_2 = 2.0  # outside subdomain to be optimized
+
+    # run the script
+
+    comm = spyro.utils.mpi_init(model)
+
+    mesh, V = spyro.io.read_mesh(model, comm)
+
+    vp = spyro.io.interpolate(model, mesh, V, guess=True)
+
+    # visualize the updates with this file
+    if comm.ensemble_comm.rank == 0:
+        evolution_of_velocity = File(
+            "../../results/evolution_of_velocity.pvd", comm=comm.comm)
+        evolution_of_velocity.write(vp, name="velocity")
+
+    # Configure the sources and receivers
+    sources = spyro.Sources(model, mesh, V, comm).create()
+
+    receivers = spyro.Receivers(model, mesh, V, comm).create()
+
+    # run the optimization based on a line search for max_iter iterations
+    vp = optimization(model, mesh, V, comm, vp,
+                      sources, receivers, max_iter=50)
+
+    return None
